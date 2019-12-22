4c64f9f439223554db23729c315d938be5c84f08
sato064 <satoberu@gmail.com>
2019-12-22 20:58:10 +0900

    Make START,END

--- Compiler.py
+++ Compiler.py
@@ -5,15 +5,21 @@ f1 = file1.readlines()
 file1.close()
 
 #Analysis
+P_Len = len(f1)
+print(P_Len)
 PG = 0
 n = 0
 
-while(PG <= 1):
+while(PG < P_Len):
     print(f1[PG])
-    print(len(f1[PG]))
+    if(f1[PG] == "START\n"):
+        print("RETURN 10000")
+    if(f1[PG] == "END"):
+        print("RETURN 10001")
+    #print(len(f1[PG]))
     n = 0
     while(len(f1[PG]) > n):
-        print(f1[PG][n])
+        #print(f1[PG][n])
         n += 1
     
     PG += 1
